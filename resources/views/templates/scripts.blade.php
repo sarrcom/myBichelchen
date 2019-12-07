b8dc81c1e7227349b3cb8873a0be297b4dccdf12
jmfg13 <42575289+jmfg13@users.noreply.github.com>
2019-12-07 15:39:52 +0100

    added views templates

---
+++ resources/views/templates/scripts.blade.php
@@ -0,0 +1,12 @@
+<!--  SCRIPTS  -->
+<!-- JQuery -->
+<script type="text/javascript" src="../../MDB/js/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="../../MDB/js/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="../../MDB/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="../../MDB/js/mdb.min.js"></script>
+<script>
+new WOW().init();
+</script>
