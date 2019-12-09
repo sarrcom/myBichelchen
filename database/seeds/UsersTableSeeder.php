242c930a8e658609f1b0e7c90be1226eeb382262
jmfg13 <42575289+jmfg13@users.noreply.github.com>
2019-12-09 13:01:27 +0100

    structure

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,63 +17,70 @@ class UsersTableSeeder extends Seeder
             'date_of_birth' => '1980-05-05',
             'username' => 'TestTeacher1',
             'password' => 'test',
-            'role' => 'Teacher',
+            'role' => 'Teacher'
         ]);
+        
         DB::table('jerd_users')->insert([
             'first_name' => 'Test2',
             'last_name' => 'Test',
             'date_of_birth' => '1980-05-05',
             'username' => 'TestTeacher2',
             'password' => 'test',
-            'role' => 'Teacher',
+            'role' => 'Teacher'
         ]);
+        
         DB::table('jerd_users')->insert([
             'first_name' => 'Test3',
             'last_name' => 'Test',
             'date_of_birth' => '1980-05-05',
             'username' => 'TestParent1',
             'password' => 'test',
-            'role' => 'Guardian',
+            'role' => 'Guardian'
         ]);
+        
         DB::table('jerd_users')->insert([
             'first_name' => 'Test4',
             'last_name' => 'Test',
             'date_of_birth' => '1980-05-05',
             'username' => 'TestParent2',
             'password' => 'test',
-            'role' => 'Guardian',
+            'role' => 'Guardian'
         ]);
+        
         DB::table('jerd_users')->insert([
             'first_name' => 'Test5',
             'last_name' => 'Test',
             'date_of_birth' => '1980-05-05',
             'username' => 'TestParent3',
             'password' => 'test',
-            'role' => 'Guardian',
+            'role' => 'Guardian'
         ]);
+        
         DB::table('jerd_users')->insert([
             'first_name' => 'Test6',
             'last_name' => 'Test',
             'date_of_birth' => '1980-05-05',
             'username' => 'TestParent4',
             'password' => 'test',
-            'role' => 'Guardian',
+            'role' => 'Guardian'
         ]);
+        
         DB::table('jerd_users')->insert([
             'first_name' => 'Test7',
             'last_name' => 'Test',
             'date_of_birth' => '1980-05-05',
             'username' => 'TestMaRe1',
             'password' => 'test',
-            'role' => 'MaRe',
+            'role' => 'MaRe'
         ]);
+        
         DB::table('jerd_users')->insert([
             'first_name' => 'Test8',
             'last_name' => 'Test',
             'date_of_birth' => '1980-05-05',
             'username' => 'TestMaRe2',
             'password' => 'test',
-            'role' => 'MaRe',
+            'role' => 'MaRe'
         ]);
     }
 }
--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,13 +1,25 @@
-    <!--/.Footer Links-->
-
-    <!--Copyright-->
-    <div class="footer-copyright py-3 text-center">
-      <div class="container-fluid">
-        © 2019 Copyright: <a href="https://mdbootstrap.com/docs/jquery/"> MDBootstrap.com </a>
-
-      </div>
+<div class="container">
+    <!--Grid row-->
+    <div class="row mb-3">
+        <!--First column-->
+        <div class="col-md-12">
+            <ul class="list-unstyled d-flex justify-content-center mb-0 py-4 list-inline">
+                <li class="list-inline-item"><a class="p-2 m-2 fa-lg fb-ic"><i class="fab fa-facebook-f white-text fa-lg"></i></a></li>
+                <li class="list-inline-item"><a class="p-2 m-2 fa-lg tw-ic"><i class="fab fa-twitter white-text fa-lg"> </i></a></li>
+                <li class="list-inline-item"><a class="p-2 m-2 fa-lg gplus-ic"><i class="fab fa-google-plus-g white-text fa-lg"></i></a></li>
+                <li class="list-inline-item"><a class="p-2 m-2 fa-lg li-ic"><i class="fab fa-linkedin-in white-text fa-lg"></i></a></li>
+                <li class="list-inline-item"><a class="p-2 m-2 fa-lg ins-ic"><i class="fab fa-instagram white-text fa-lg"></i></a></li>
+                <li class="list-inline-item"><a class="p-2 m-2 fa-lg pin-ic"><i class="fab fa-pinterest white-text fa-lg"></i></a></li>
+            </ul>
+        </div>
+        <!--/First column-->
     </div>
-    <!--/.Copyright-->
-
-  </footer>
-  <!--/.Footer-->
+    <!--/Grid row-->
+</div>
+<!--/.Footer Links-->
+<!--Copyright-->
+<div class="footer-copyright py-3 text-center">
+    <div class="container-fluid">© 2019 Copyright: <a href="https://mdbootstrap.com/docs/jquery/"> MDBootstrap.com </a>
+    </div>
+</div>
+<!--/.Copyright-->
