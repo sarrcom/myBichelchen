87b0ae7a1c1ac4b3d4c9b4589176827eea60086a
deburghardt <55229904+deburghardt@users.noreply.github.com>
2019-12-10 11:53:09 +0100

    responsible table

--- database/seeds/ResponsibleOfStudentsTableSeeder.php
+++ database/seeds/ResponsibleOfStudentsTableSeeder.php
@@ -13,7 +13,171 @@ class ResponsibleOfStudentsTableSeeder extends Seeder
     {
         DB::table('jerd_responsible_of_students')->insert([
             'student_id' => 1,
-            'user_id' => 1,
+            'user_id' => 3,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 1,
+            'user_id' => 4,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 1,
+            'user_id' => 7,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 2,
+            'user_id' => 5,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 2,
+            'user_id' => 7,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 3,
+            'user_id' => 5,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 3,
+            'user_id' => 7,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 4,
+            'user_id' => 5,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 4,
+            'user_id' => 7,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 5,
+            'user_id' => 6,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 5,
+            'user_id' => 8,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 6,
+            'user_id' => 6,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 6,
+            'user_id' => 8,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 7,
+            'user_id' => 4,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 7,
+            'user_id' => 3,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 7,
+            'user_id' => 8,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 8,
+            'user_id' => 6,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 8,
+            'user_id' => 8,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 9,
+            'user_id' => 5,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 9,
+            'user_id' => 8,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 10,
+            'user_id' => 3,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 10,
+            'user_id' => 4,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 10,
+            'user_id' => 8,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 11,
+            'user_id' => 5,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 11,
+            'user_id' => 8,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 12,
+            'user_id' => 5,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 12,
+            'user_id' => 7,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 13,
+            'user_id' => 3,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 13,
+            'user_id' => 4,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 13,
+            'user_id' => 7,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 14,
+            'user_id' => 5,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 14,
+            'user_id' => 7,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 15,
+            'user_id' => 6,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 15,
+            'user_id' => 8,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 16,
+            'user_id' => 3,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 16,
+            'user_id' => 6,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 16,
+            'user_id' => 8,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 17,
+            'user_id' => 6,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 17,
+            'user_id' => 7,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 18,
+            'user_id' => 3,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 18,
+            'user_id' => 5,
+        ]);
+        DB::table('jerd_responsible_of_students')->insert([
+            'student_id' => 18,
+            'user_id' => 8,
         ]);
     }
 }
