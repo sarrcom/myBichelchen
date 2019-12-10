8a2cd0f72ede81a9970e10f577b6e217715897bd
jmfg13 <42575289+jmfg13@users.noreply.github.com>
2019-12-10 10:28:59 +0100

    Update add-user.blade.php

--- resources/views/admin/add-user.blade.php
+++ resources/views/admin/add-user.blade.php
@@ -1,11 +1,9 @@
-<form>
+<form method="POST">
     @csrf
     @method('POST')
     <input type="text" name="first_name" placeholder="First Name">
     <input type="text" name="last_name" placeholder="Last Name">
     <input type="date" name="date_of_birth">
-    <input type="text" name="username" placeholder="username">
-    <input type="password" name="password" placeholder="password">
     <select name="role">
         <option value="Teacher">Teacher</option>
         <option value="Guardian">Guardian</option>
