3db59d53f1abdc2e06632a1ffbe40d325d8e4b91
R <55230013+rvw2019@users.noreply.github.com>
2019-12-11 14:14:07 +0100

    making it multilingual

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,5 +1,36 @@
 <?php
 
 return [
-    'welcome'       => 'Welcome to our application'
+
+    'slug' => 'No More Paper Forms Makes Everyone Happy!',
+
+    'sign_in' => 'Sign in',
+    'contact_us' => 'Contact us',
+    'your_username' => 'Your username',
+    'your_password' => 'Your password',
+    'forgot_password' => 'Forgot password?',
+
+    'welcome_h_1' => 'What is myBichelchen?',
+    'welcome_p_1' => ' is a digital version of the \'Bichelchen\' - the agenda the teachers use to note the students\' homework in primary school and to communicate with parents.',
+    'welcome_p_2' => ' allows an administrator to create teachers, parents, and day care attendants (known as Maison Relais).',
+    'welcome_p_3' => 'Parents, teachers, and day care attendants can see their children\'s homework, notes, planned activities, and manage absences.',
+
+    'testimonials' => 'Testimonials',
+    'tell_us_why' => 'Tell us why you love myBichelchen!',
+    'profession_1' => 'Teacher',
+    'testimonial_1' => 'myBichelchen saves me an hour per day and allows me to concentrate on the important stuff.',
+    'profession_2' => 'Day care attendant',
+    'testimonial_2' => 'Established a long time ago, I somehow got tangled up in this. I don\'t know how, but I like it.',
+    'profession_3' => 'Parent',
+    'testimonial_3' => 'This completely threw my life around 360 degrees. Now I finally know where I\'m heading.',
+
+    'contact_msg' => 'Drop us a message, we\'d love to hear from you!',
+    'your_name' => 'Your name',
+    'your_email' => 'Your email',
+    'your_subject' => 'Subject',
+    'your_message' => 'Your message',
+    'send' => 'Send'
+
 ];
+
+?>
--- resources/lang/fr/messages.php
+++ resources/lang/fr/messages.php
@@ -1,5 +1,36 @@
 <?php
 
 return [
-    'welcome'       => 'Bienvenue!'
+
+    'slug' => 'Plus de formulaires papier, ça rend tout le monde heureux !',
+
+    'sign_in' => 'Se connecter',
+    'contact_us' => 'Nous contacter',
+    'your_username' => 'Votre username',
+    'your_password' => 'Votre password',
+    'forgot_password' => 'Forgot password?',
+
+    'welcome_h_1' => 'Qu\'est-ce que myBichelchen ?',
+    'welcome_p_1' => ' est une version numérique du \'Bichelchen\' - l\'agenda utilisé par les enseignants pour noter les devoirs des élèves du primaire et pour communiquer avec les parents.',
+    'welcome_p_2' => ' permet à un administrateur de créer des enseignants, des parents et des responsables de garderie (Maison Relais).',
+    'welcome_p_3' => 'Les parents, les enseignants et les préposés de garderie peuvent voir les devoirs, les notes, les activités prévues et gérer les absences de leurs enfants.',
+
+    'testimonials' => 'Témoignages',
+    'tell_us_why' => 'Dites-nous pourquoi vous aimez myBichelchen !',
+    'profession_1' => 'Enseignant',
+    'testimonial_1' => 'myBichelchen me fait gagner une heure par jour et me permet de me concentrer sur les choses importantes.',
+    'profession_2' => 'Maison Relais',
+    'testimonial_2' => 'Créé il y a longtemps, je me suis empêtré là-dedans. Je ne sais pas comment, mais ça me plaît.',
+    'profession_3' => 'Parent',
+    'testimonial_3' => 'Cela a complètement bouleversé ma vie à 360 degrés. Maintenant, je sais enfin où je vais.',
+
+    'contact_msg' => 'Envoyez-nous un message, nous aimerions avoir de vos nouvelles !',
+    'your_name' => 'Votre nom',
+    'your_email' => 'Votre email',
+    'your_subject' => 'Suject',
+    'your_message' => 'Votre message',
+    'send' => 'Envoyer',
+
 ];
+
+?>
