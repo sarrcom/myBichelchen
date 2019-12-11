3fd44b7abaa9aed66a841c60d342a7c0c92ed464
kimchi <55229423+sukmabols@users.noreply.github.com>
2019-12-11 10:40:22 +0100

    replaced css files

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -29,9 +29,9 @@
     <footer>
         @include('templates.footer')
     </footer>
-        @include('templates.scripts')
-        @yield('extra-scripts')
-        {{-- Google Analytics --}}
-        @include('templates.google_anal')
+    @include('templates.scripts')
+    @yield('extra-scripts')
+    {{-- Google Analytics --}}
+    @include('templates.google_anal')
 </body>
 </html>
