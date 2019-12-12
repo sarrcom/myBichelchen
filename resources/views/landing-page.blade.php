435c2eb21da8fa75f802ed5afd18f9f10753d289
kimchi <55229423+sukmabols@users.noreply.github.com>
2019-12-12 16:33:26 +0100

    Update landing-page.blade.php

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -5,25 +5,24 @@
 @section('navbar')
 @include('templates.navbar')
 @endsection
-    <!-- Header Background Image with Logo, Sign in and Contact us -->
-    <div class="view jarallax" data-jarallax='{"speed": 0.2}' style="background-image: url('../../MDB/img/AdobeStock_190123382_Preview.png'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
-        <div class="mask rgba-white-slight">
-            <div class="container h-100 d-flex justify-content-center align-items-center">
-                <div class="row pt-5 mt-3">
-                    <div class="col-md-12 wow fadeIn mb-3">
-                        <div class="text-center">
-                            <h1 class="display-4 font-weight-bold mb-5 wow fadeInUp"><img id="logo" src="../../MDB/img/logo.PNG" alt=""></h1>
-                            <h5 class="mb-5 wow fadeInUp" data-wow-delay="0.2s">{{ __('landing-page.slug') }}</h5>
-                            <div class="wow fadeInUp" data-wow-delay="0.4s">
-                                <a class="btn btn-orange btn-rounded" data-toggle="modal" data-target="#elegantModalForm"><i class="fas fa-user left"></i>{{ __('landing-page.sign_in') }}</a>
-                                <a href="#contact" class="btn btn-outline-orange btn-rounded"><i class="fas fa-book left"></i>{{ __('landing-page.contact_us') }}</a>
-                            </div>
+<div class="view jarallax" data-jarallax='{"speed": 0.2}' style="background-image: url('../../MDB/img/AdobeStock_190123382_Preview.png'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+    <div class="mask rgba-white-slight">
+        <div class="container h-100 d-flex justify-content-center align-items-center">
+            <div class="row pt-5 mt-3">
+                <div class="col-md-12 wow fadeIn mb-3">
+                    <div class="text-center">
+                        <h1 class="display-4 font-weight-bold mb-5 wow fadeInUp"><img id="logo" src="../../MDB/img/logo.PNG" alt=""></h1>
+                        <h5 class="mb-5 wow fadeInUp" data-wow-delay="0.2s">{{ __('landing-page.slug') }}</h5>
+                        <div class="wow fadeInUp" data-wow-delay="0.4s">
+                            <a class="btn btn-orange btn-rounded" data-toggle="modal" data-target="#elegantModalForm"><i class="fas fa-user left"></i>{{ __('landing-page.sign_in') }}</a>
+                            <a href="#contact" class="btn btn-outline-orange btn-rounded"><i class="fas fa-book left"></i>{{ __('landing-page.contact_us') }}</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </header>
 
 @section('content')
