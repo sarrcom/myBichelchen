39b8319501df6bfc9aa86a9181023af59889ebcc
kimchi <55229423+sukmabols@users.noreply.github.com>
2019-12-13 15:21:00 +0100

    edit form appeared again and i fixed again

--- resources/views/admin/users/users-list.blade.php
+++ resources/views/admin/users/users-list.blade.php
@@ -185,7 +185,7 @@
     });
     $('.dataTables_length').addClass('bs-select');
     //Add the class 'active'on the Edit form inputs so it doesn't overlap the pre-existing fields
-    $('#editButton').on('click', function(e) {
+    $('#editOpenModal').on('click', function () {
         $('#editForm label').addClass('active');
     })
 </script>
