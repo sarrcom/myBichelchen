c9764fdc8fc9cc8eadfc291c541258a281f405a1
elainekim85 <55229423+elainekim85@users.noreply.github.com>
2019-12-14 22:05:07 +0100

    Update messages.blade.php

--- resources/views/users/teacher/messages.blade.php
+++ resources/views/users/teacher/messages.blade.php
@@ -40,8 +40,9 @@
         
                         <li class="active grey lighten-3 p-2">
                             <a href="#" class="d-flex justify-content-between">
+                            <img src="https://image.flaticon.com/icons/png/512/145/145842.png" alt="avatar" class="avatar rounded-circle d-flex align-self-center mr-2 z-depth-1">
                             <div class="text-small">
-                                <strong>Elaine Kim</strong>
+                                <strong>Sebastian McLloyd</strong>
                                 <p class="last-message text-muted">
                                     Hello, Are you there?
                                 </p>
@@ -55,6 +56,7 @@
                         
                         <li class="p-2">
                             <a href="#" class="d-flex justify-content-between">
+                            <img src="https://image.flaticon.com/icons/png/512/145/145844.png" alt="avatar" class="avatar rounded-circle d-flex align-self-center mr-2 z-depth-1">
                                 <div class="text-small">
                                     <strong>Elaine Kim</strong>
                                     <p class="last-message text-muted">
@@ -78,45 +80,58 @@
             <!-- Selected Message -->
             <!-- Grid column -->
             <div class="col-md-6 col-xl-8 pl-md-3 px-lg-auto px-0">
+                <h6 class="font-weight-bold mb-3 text-center text-lg-left"><label for="sendTo">Send To: </label></h6>
+                <ul class="list-unstyled chat">
+            <li class="d-flex justify-content-between mb-4">
+              <img src="https://image.flaticon.com/icons/png/512/145/145843.png" alt="avatar" class="avatar rounded-circle mr-2 ml-lg-3 ml-0 z-depth-1">
+              <div class="chat-body white p-3 ml-2 z-depth-1">
+                <div class="header">
+                  <strong class="primary-font">Marius Dobreanu</strong>
+                  <small class="pull-right text-muted"><i class="far fa-clock"></i> 14 mins ago</small>
+                </div>
+                <hr class="w-100">
+                <p class="mb-0">
+                  Hello, how many pages does my son have to write for his homework today?
+                </p>
+              </div>
+            </li>
 
-                <div class="chat-message">
+            <li class="d-flex justify-content-between mb-4">
+              <div class="chat-body white p-3 z-depth-1">
+                <div class="header">
+                  <strong class="primary-font">Sebastian McLloyd</strong>
+                  <small class="pull-right text-muted"><i class="far fa-clock"></i> 13 mins ago</small>
+                </div>
+                <hr class="w-100">
+                <p class="mb-0">
+                  Just the three as I have mentioned in the notes, please make sure he goes over the extra classes I've assigned for him.
+                </p>
+              </div>
+              <img src="https://image.flaticon.com/icons/png/512/145/145842.png" alt="avatar" class="avatar rounded-circle mr-0 ml-3 z-depth-1">
+            </li>
 
-                <ul class="list-unstyled chat-1 scrollbar-light-blue">
+            <li class="d-flex justify-content-between mb-4 pb-3">
+              <img src="https://image.flaticon.com/icons/png/512/145/145843.png" alt="avatar" class="avatar rounded-circle mr-2 ml-lg-3 ml-0 z-depth-1">
+              <div class="chat-body white p-3 ml-2 z-depth-1">
+                <div class="header">
+                  <strong class="primary-font">Marius Dobreanu</strong>
+                  <small class="pull-right text-muted"><i class="far fa-clock"></i> 12 mins ago</small>
+                </div>
+                <hr class="w-100">
+                <p class="mb-0">
+                  No problem at all!
+                </p>
+              </div>
+            </li>
 
-                    <li class="d-flex justify-content-between mb-4">
-                        <div class="chat-body white p-3 ml-2 z-depth-1">
-                            <div class="header">
-                                <strong class="primary-font">Elaine Kim</strong>
-                                <small class="pull-right text-muted"><i class="far fa-clock"></i> 12 mins ago</small>
-                            </div>
-                            <hr class="w-100">
-                            <p class="mb-0">
-                            I'm so confused.
-                            </p>
-                        </div>
-                    </li>
-
-                    <li class="d-flex justify-content-between mb-4">
-                    <div class="chat-body white p-3 z-depth-1">
-                        <div class="header">
-                            <strong class="primary-font">Elaine Kim</strong>
-                            <small class="pull-right text-muted"><i class="far fa-clock"></i> 13 mins ago</small>
-                        </div>
-                        <hr class="w-100">
-                        <p class="mb-0">
-                        What's supposed to be here? Send Help
-                        </p>
-                    </div>
-                    
-                </ul>
+            <li class="white">
                 <!-- Send Message -->
                 <div class="chat-body white p-3 z-depth-1">
-                    <div class="form-group basic-textarea">
+                    
                         <form method="POST" id="sendMessageForm">
                             @csrf
                             @method('POST')
                             
-                            <strong class="primary-font"><label for="sendTo">Send To: </label></strong>
                             <input type="radio" name="sendTo" value="class" checked>Class
                             <input type="radio" name="sendTo" value="student">One Student<br>
                             
@@ -130,11 +145,12 @@
                             <input type="text" name="subject" required><br>
                             
                         </form>
-
+                    <div class="form-group basic-textarea">
                         <textarea name="description" class="form-control pl-2 my-0" id="exampleFormControlTextarea2" rows="3" placeholder="Type your message here..." required></textarea>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-outline-white btn-rounded btn-sm waves-effect waves-dark float-right" name="submitHomework">Send</button>
+            </li>
+                <button type="submit" class="btn btn-info btn-rounded btn-sm waves-effect waves-light float-right" name="submitHomework">Send</button>
 
                 </div>
             </div>
@@ -142,10 +158,12 @@
             <!-- Selected Message -->
         </div>
         <!-- Grid row -->
-
+        
         </div>
     </div>
 </div>
+<!-- https://www.flaticon.com/packs/avatar-set -->
+<div class="text-center">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 @endsection
 
 @section('footer')
