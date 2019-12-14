9831e44133146ee32be33928f86fc30dbe650b94
elainekim85 <55229423+elainekim85@users.noreply.github.com>
2019-12-14 20:10:28 +0100

    Update messages.blade.php

--- resources/views/users/teacher/messages.blade.php
+++ resources/views/users/teacher/messages.blade.php
@@ -33,16 +33,18 @@
             <!-- Grid column -->
             <div class="col-md-6 col-xl-4 px-0">
 
-                <h6 class="font-weight-bold mb-3 text-center text-lg-left">myBichelchen Member</h6>
+                <h6 class="font-weight-bold mb-3 text-center text-lg-left">Inbox</h6>
                 <div class="white z-depth-1 px-2 pt-3 pb-0 members-panel-1 scrollbar-light-blue">
                     
-                    <ul class="list-unstyled friend-list">
-
+                    <ul id="allMessages" class="list-unstyled friend-list">
+        
                         <li class="active grey lighten-3 p-2">
                             <a href="#" class="d-flex justify-content-between">
                             <div class="text-small">
-                                <strong>John Doe</strong>
-                                <p class="last-message text-muted">Hello, Are you there?</p>
+                                <strong>Elaine Kim</strong>
+                                <p class="last-message text-muted">
+                                    Hello, Are you there?
+                                </p>
                             </div>
                             <div class="chat-footer">
                                 <p class="text-smaller text-muted mb-0">Just now</p>
@@ -50,13 +52,13 @@
                             </div>  
                             </a>
                         </li>
-
+                        
                         <li class="p-2">
                             <a href="#" class="d-flex justify-content-between">
                                 <div class="text-small">
-                                    <strong>Danny Smith</strong>
+                                    <strong>Elaine Kim</strong>
                                     <p class="last-message text-muted">
-                                        <ul id="allMessages"></ul>
+                                        Am I in the Matrix?
                                     </p>
                                 </div>
                                 <div class="chat-footer">
@@ -83,15 +85,13 @@
 
                     <li class="d-flex justify-content-between mb-4">
                         <div class="chat-body white p-3 ml-2 z-depth-1">
-                        
                             <div class="header">
-                                <strong class="primary-font">Brad Pitt</strong>
+                                <strong class="primary-font">Elaine Kim</strong>
                                 <small class="pull-right text-muted"><i class="far fa-clock"></i> 12 mins ago</small>
                             </div>
                             <hr class="w-100">
                             <p class="mb-0">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua.
+                            I'm so confused.
                             </p>
                         </div>
                     </li>
@@ -99,47 +99,44 @@
                     <li class="d-flex justify-content-between mb-4">
                     <div class="chat-body white p-3 z-depth-1">
                         <div class="header">
-                            <strong class="primary-font">Lara Croft</strong>
+                            <strong class="primary-font">Elaine Kim</strong>
                             <small class="pull-right text-muted"><i class="far fa-clock"></i> 13 mins ago</small>
                         </div>
                         <hr class="w-100">
                         <p class="mb-0">
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                        laudantium.
+                        What's supposed to be here? Send Help
                         </p>
                     </div>
                     
                 </ul>
                 <!-- Send Message -->
-                <div class="white">
+                <div class="chat-body white p-3 z-depth-1">
                     <div class="form-group basic-textarea">
-                    <form method="POST" id="sendMessageForm">
-                        @csrf
-                        @method('POST')
-                        
-                        <label for="sendTo">Send To</label>
-                        <input type="radio" name="sendTo" value="class" checked>Class
-                        <input type="radio" name="sendTo" value="student">One Student<br>
-                        
-                        <label for="recipient">Recipient</label>
-                        
-                        {{-- options from script based on the radio: classes or students--}}
-                        <select name="recipient" id="recipient" required>
-                        </select><br>
+                        <form method="POST" id="sendMessageForm">
+                            @csrf
+                            @method('POST')
                             
-                        <label for="subject">Subject</label>
-                        <input type="text" name="subject" required><br>
-                        
-                        <input type="submit" name="submitHomework" value="Submit">
-                    </form>
+                            <strong class="primary-font"><label for="sendTo">Send To: </label></strong>
+                            <input type="radio" name="sendTo" value="class" checked>Class
+                            <input type="radio" name="sendTo" value="student">One Student<br>
+                            
+                            <label for="recipient">Recipient</label>
+                            
+                            {{-- options from script based on the radio: classes or students--}}
+                            <select name="recipient" id="recipient" required>
+                            </select><br>
+                                
+                            <label for="subject">Subject</label>
+                            <input type="text" name="subject" required><br>
+                            
+                        </form>
 
-                    <textarea name="description" class="form-control pl-2 my-0" id="exampleFormControlTextarea2" rows="3" placeholder="Type your message here..." required></textarea>
+                        <textarea name="description" class="form-control pl-2 my-0" id="exampleFormControlTextarea2" rows="3" placeholder="Type your message here..." required></textarea>
                     </div>
                 </div>
-                <button type="button" class="btn btn-outline-white btn-rounded btn-sm waves-effect waves-dark float-right">Send</button>
+                <button type="submit" class="btn btn-outline-white btn-rounded btn-sm waves-effect waves-dark float-right" name="submitHomework">Send</button>
 
                 </div>
-
             </div>
             <!-- Grid column -->
             <!-- Selected Message -->
@@ -154,7 +151,8 @@
 @section('footer')
 @include('templates.footer')
 @endsection
-<form method="POST" id="sendMessageForm">
+
+<!-- <form method="POST" id="sendMessageForm">
     @csrf
     @method('POST')
     
@@ -176,8 +174,7 @@
     
     <input type="submit" name="submitHomework" value="Submit">
 </form>
-
-
+-->
 
 @include('templates.scripts')
 <script>
