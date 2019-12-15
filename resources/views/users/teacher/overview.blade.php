541f59cff6ccb3907bcb65863bb9865f18f4ed5c
elainekim85 <55229423+elainekim85@users.noreply.github.com>
2019-12-15 16:00:19 +0100

    whoopsie daisy

--- resources/views/users/teacher/overview.blade.php
+++ resources/views/users/teacher/overview.blade.php
@@ -66,7 +66,7 @@
             <div class="view view-cascade gradient-card-header peach-gradient">
 
                 <!-- Title -->
-                <a href="#!" class="white-text">
+                <a href="/user/messages" class="white-text">
                     <h2 class="card-header-title mb-3">Notes</h2>
                 </a>
                 <!-- Subtitle -->
@@ -79,7 +79,7 @@
                 <!-- Text -->
                 <p class="card-text">Today's Notes Logic goes here</p>
                 <!-- Link -->
-                <a href="#!" class="orange-text d-flex flex-row-reverse p-2">
+                <a href="/user/messages" class="orange-text d-flex flex-row-reverse p-2">
                     <h5 class="waves-effect waves-light">Read more<i class="fas fa-angle-double-right ml-2"></i></h5>
                 </a>
             </div>
@@ -121,91 +121,8 @@
 </div>
 <!-- /Notifications -->
 
-<br><br><br>
 
-<!-- Unread Messages -->
-<div class="container">
-    <!-- Table with panel -->
-    <div class="card card-cascade narrower">
 
-    <!--Card image-->
-    <div
-        class="view view-cascade gradient-card-header blue-gradient narrower py-2 mx-4 mb-3 d-flex justify-content-between align-items-center">
-
-        <div>
-            <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
-                <i class="far fa-envelope"></i>
-            </button>
-        </div>
-
-        <a href="/user/messages" class="white-text mx-3">Unread Messages</a>
-
-        <div>
-            <button type="button" class="btn btn-outline-white btn-rounded btn-sm px-2">
-                <i class="far fa-trash-alt mt-0"></i>
-            </button>
-        </div>
-
-    </div>
-    <!--/Card image-->
-
-    <div class="px-4">
-
-        <div class="table-wrapper">
-        <!--Table-->
-        <table class="table table-hover mb-0">
-
-            <!--Table head-->
-            <thead>
-            <tr>
-                    <th>
-                        <input class="form-check-input" type="checkbox" id="checkbox">
-                        <label class="form-check-label" for="checkbox" class="mr-2 label-table"></label>
-                    </th>
-                <th class="th-lg">
-                    <a>First Name</a>
-                </th>
-                <th class="th-lg">
-                    <a href="">Last Name</a>
-                </th>
-                <th class="th-lg">
-                    <a href="">Username</a>
-                </th>
-                <th class="th-lg">
-                    <a href="">Message</a>
-                </th>
-                <th class="th-lg">
-                    <a href="">Date</a>
-                </th>
-            </tr>
-            </thead>
-            <!--/Table head-->
-
-            <!--Table body-->
-            <tbody>
-                <tr>
-                    <th scope="row">
-                    <input class="form-check-input" type="checkbox" id="checkbox1">
-                    <label class="form-check-label" for="checkbox1" class="label-table"></label>
-                    </th>
-                    <td>Elaine</td>
-                    <td>Kim</td>
-                    <td>clusterfox</td>
-                    <td>Hello are you there</td>
-                    <td>{{date("d/m/Y")}}</td>
-                </tr>
-            </tbody>
-            <!--/Table body-->
-        </table>
-        <!--/Table-->
-        </div>
-
-    </div>
-
-    </div>
-    <!-- Table with panel -->
-</div>
-<!-- /Unread Meassages -->
 
 @endsection
 
