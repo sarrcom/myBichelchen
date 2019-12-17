d5a482f2ecddd5f8fbe5c2cf99757cfc71909479
kimchi <55229423+sukmabols@users.noreply.github.com>
2019-12-17 11:00:43 +0100

    Update homework.blade.php

--- resources/views/users/teacher/homework.blade.php
+++ resources/views/users/teacher/homework.blade.php
@@ -15,108 +15,80 @@
     <br><br><br>
 </div>
 
-
 <div class="container">
         <div class="card night-fade-gradient chat-room">
-            
+
             <div class="card-body">
                 <!-- Grid row -->
                 <div class="row px-lg-2 px-2">
-                    
                     <!-- Grid column -->
-                    <div class="col-md-6 col-xl-4 px-0">
-                        
-                        <h6 class="font-weight-bold mb-3 text-center text-lg-left">Homework Calendar</h6>
-                        
-                        <button class="btn btn-purple btn-rounded btn-sm waves-effect waves-light" id="previous"><i class="fas fa-caret-left"></i></button> <button class="btn btn-purple btn-rounded btn-sm waves-effect waves-light" id="next"><i class="fas fa-caret-right"></i></button>
-                        <!-- Homework Calender -->
-                        <div class="homeworkcalendar">
-                        </div>
-                    </div>
-                    <!-- /Grid column -->
-                    
-                    <!-- Grid column -->
-                    <div class="col-md-6 col-xl-8 pl-md-3 px-lg-auto px-0">
-                        
+                    <div class="col-md-6 col-xl-8 pl-md-3 px-lg-auto px-0 mx-auto">
+
                         <ul class="list-unstyled chat">
                             <!-- Homework Box -->
-                            <li class="d-flex justify-content-between mb-4 pb-3">
-                                <div class="chat-body white p-3 ml-2 z-depth-1">
+                            <li class="d-flex justify-content-center">
+                                <div class="chat-body white p-3 z-depth-1" style="width: 80vh">
                                     <div class="header">
-                                        <strong class="primary-font">Homework</strong><br>
-                                        <label for="duedate">Due date:</label><br>
-                                        <label for="subject">Subject</label>
+                                        <h6 class="font-weight-bold mb-2 text-center text-lg-center">Homework Calendar</h6>
+                                        <hr class="w-100">
+                                        <!-- Homework Calender -->
+                                        <div class="homeworkcalendar">
+                                        </div>
+                                        <hr class="w-100">
                                     </div>
-                                    <hr class="w-100">
-                                    <p class="mb-0">
-                                        Write an essay on bananas.
-                                    </p>
                                 </div>
                             </li>
                             <!-- /Homework Box -->
-                            
+                            <li class="d-flex justify-content-center">
+                                <button class="btn btn-info btn-rounded btn-sm waves-effect waves-light" id="previous"><i class="fas fa-caret-left"></i></button>
+                                <button class="btn btn-info btn-rounded btn-sm waves-effect waves-light" id="next"><i class="fas fa-caret-right"></i></button>
+                            </li>
                             <!-- Send Homework -->
                             <div id="status">
                             </div>
                             <form method="POST" id="homeworkForm">
                                 @csrf
                                 @method('POST')
-                                
+
                                 <div class="chat-body white p-3 z-depth-1">
-                                    
+
                                     <ng-container [formGroup]="testForm">
-                                        
+
                                         <div class="custom-control custom-switch">
                                             <input type="checkbox" class="custom-control-input" id="customSwitches" name="sendTo" formControlName="switchControl" value="class">
                                             <label class="custom-control-label" for="customSwitches">Toggle to switch between Classes and Students</label>
                                         </div>
                                         <hr class="w-100">
-    
+
                                     </ng-container>
-    
+
                                     <label for="recipient">To the Parents of</label>
-    
+
                                     {{-- options from script based on the radio: classes or students--}}
                                     <select name="recipient" id="recipient" required>
                                     </select><br>
-    
+
                                     <label for="subject">Subject</label>
                                     <input type="text" name="subject" required><br>
-    
+
                                     <div class="form-group basic-textarea">
                                         <textarea name="description" class="form-control pl-2 my-0" id="exampleFormControlTextarea2" rows="3" placeholder="Type your message here..." required></textarea>
                                     </div>
-                                    
+
                                     <label for="dueDate">Due Date</label>
                                     <input type="date" name="dueDate" required><br>
 
                                 </div>
-                                
 
-                                <button type="submit" class="btn btn-blue btn-rounded btn-sm waves-effect waves-light float-right" name="submitHomework">Send</button>
-    
+
+                                <button type="submit" class="btn btn-info btn-rounded btn-sm waves-effect waves-light float-right" name="submitHomework">Send</button>
+
                             </form>
                     <!-- Grid column -->
                 </div>
                 <!-- Grid row -->
             </div>
         </div>
-
-<div class="container">
-    <div class="d-flex justify-content-center">
-        <p class="h5 text-primary createShowP">
-        </p>
-        <br><br><br>
-    </div>
-    <div class="card night-fade-gradient">
-
-        <div class="homeworkcalendar">
-        </div>
-
-    </div>
-</div>
-<button id="testerino">Homework of student 1</button>
-
 @endsection
 
 @include('templates.scripts')
@@ -133,7 +105,7 @@
     $('#customSwitches').click(changeRecipient);
 
     $('#testerino').click(test);
-    
+
     //show the date of the tables from today and the next 5 schooldays;
     //by clicking the buttomn and changing the page variable you can scroll 5 schooldays back/forward
     function showTables(){
@@ -198,7 +170,7 @@
     //function to change the selector of recipient of homework
     function changeRecipient(){
         $(recipient).empty();
-        
+
 
         if ($('#customSwitches').is(":checked")) {
             @foreach($user->klasses as $klass)
@@ -275,7 +247,7 @@ function test() {
         url: '/test',
         type: 'get',
         success: function(result){
-                
+
         },
         error: function(err){
             console.log(err)
