2affa22b6dc510aa5d6d9f00c4a6224151a4dc7d
kimchi <55229423+sukmabols@users.noreply.github.com>
2019-12-18 10:14:13 +0100

    commenting out the placeholders

--- resources/views/users/mare/messages.blade.php
+++ resources/views/users/mare/messages.blade.php
@@ -12,7 +12,9 @@
 <h1 class="d-flex justify-content-center">Messages</h1>
 <div class="d-flex justify-content-center">
     <p class="h5 text-primary createShowP">{{date("l")}}, {{date("d/m/Y")}}</p>
-    <br><br><br>
+    <br>
+    <br>
+    <br>
 </div>
 
 <div class="container">
@@ -22,40 +24,104 @@
             <!-- Grid row -->
             <div class="row px-lg-2 px-2">
 
-            <!-- Grid column -->
-            <div class="col-md-6 col-xl-4 px-0">
+                <!-- Grid column -->
+                <div class="col-md-6 col-xl-4 px-0">
 
-                <h6 class="font-weight-bold mb-3 text-center text-lg-left">Inbox</h6>
-                <!-- Scroll bar -->
-                <div class="white z-depth-1 px-2 pt-3 pb-0 mb-3 members-panel-1 scrollbar-light-blue" id="homeworkList">
-                    <ul class="list-unstyled friend-list">
-                        <li class="p-2" id="allMessages">
-                            <a href="#" class="d-flex justify-content-between">
-                            <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-8.jpg" alt="avatar" class="avatar rounded-circle d-flex align-self-center mr-2 z-depth-1">
-                            <div class="text-small">
-                                <strong>John Doe</strong>
-                                <p class="last-message text-muted">Hello, Are you there?</p>
-                            </div>
-                            <div class="chat-footer">
-                                <p class="text-smaller text-muted mb-0">Just now</p>
-                                <span class="badge badge-danger float-right">1</span>
-                            </div>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-
-            </div>
-            <!-- Grid column -->
-
-            <!-- Grid column -->
-            <div class="col-md-6 col-xl-8 pl-md-3 px-lg-auto px-0">
-
-                <div class="chat-message">
+                    <h6 class="font-weight-bold mb-3 text-center text-lg-left">Inbox</h6>
                     <!-- Scroll bar -->
-                    <ul class="list-unstyled chat-1 scrollbar-light-blue">
+                    <div class="white z-depth-1 px-2 pt-3 pb-0 mb-3 members-panel-1 scrollbar-light-blue" id="homeworkList">
+                        <ul class="list-unstyled friend-list">
 
-                        <li class="d-flex justify-content-between mb-4">
+                            <li class="p-2" id="allMessages">
+                                <!-- Example of inbox items -->
+                                <!-- <a href="#" class="d-flex justify-content-between">
+                                    <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-8.jpg" alt="avatar" class="avatar rounded-circle d-flex align-self-center mr-2 z-depth-1">
+                                    <div class="text-small">
+                                        <strong>John Doe</strong>
+                                        <p class="last-message text-muted">Hello, Are you there?</p>
+                                    </div>
+                                    <div class="chat-footer">
+                                        <p class="text-smaller text-muted mb-0">Just now</p>
+                                        <span class="badge badge-danger float-right">1</span>
+                                    </div>
+                                </a> -->
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-6 col-xl-8 pl-md-3 px-lg-auto px-0">
+
+                    <div class="chat-message">
+                        <!-- Scroll bar -->
+                        <ul class="list-unstyled chat-1 scrollbar-light-blue">
+                            <!-- Example of chat -->
+
+                            <!-- <li class="d-flex justify-content-between mb-4">
+
+                                <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-6.jpg" alt="avatar" class="avatar rounded-circle mr-2 ml-lg-3 ml-0 z-depth-1">
+                                <div class="chat-body white p-3 ml-2 z-depth-1">
+                                    <div class="header">
+                                        <strong class="primary-font">Brad Pitt</strong>
+                                        <small class="pull-right text-muted"><i class="far fa-clock"></i> 12 mins ago</small>
+                                    </div>
+                                        <hr class="w-100">
+                                        <p class="mb-0">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                        labore et dolore magna aliqua.
+                                    </p>
+                                </div>
+                            </li>
+
+                            <li class="d-flex justify-content-between mb-4">
+                            <div class="chat-body white p-3 z-depth-1">
+                                <div class="header">
+                                    <strong class="primary-font">Lara Croft</strong>
+                                    <small class="pull-right text-muted"><i class="far fa-clock"></i> 13 mins ago</small>
+                                </div>
+                                    <hr class="w-100">
+                                    <p class="mb-0">
+                                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                    laudantium.
+                                </p>
+                                </div>
+                                <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-5.jpg" alt="avatar" class="avatar rounded-circle mr-0 ml-3 z-depth-1">
+                            </li>
+
+                            <li class="d-flex justify-content-between mb-4">
+                                <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-6.jpg" alt="avatar" class="avatar rounded-circle mr-2 ml-lg-3 ml-0 z-depth-1">
+                                <div class="chat-body white p-3 ml-2 z-depth-1">
+                                    <div class="header">
+                                        <strong class="primary-font">Brad Pitt</strong>
+                                        <small class="pull-right text-muted"><i class="far fa-clock"></i> 12 mins ago</small>
+                                    </div>
+                                        <hr class="w-100">
+                                        <p class="mb-0">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                                        labore et dolore magna aliqua.
+                                    </p>
+                                </div>
+                            </li>
+
+                            <li class="d-flex justify-content-between mb-4">
+                            <div class="chat-body white p-3 z-depth-1">
+                                <div class="header">
+                                    <strong class="primary-font">Lara Croft</strong>
+                                    <small class="pull-right text-muted"><i class="far fa-clock"></i> 13 mins ago</small>
+                                </div>
+                                    <hr class="w-100">
+                                    <p class="mb-0">
+                                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                        laudantium.
+                                    </p>
+                                </div>
+                            <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-5.jpg" alt="avatar" class="avatar rounded-circle mr-0 ml-3 z-depth-1">
+                            </li>
+
+                            <li class="d-flex justify-content-between mb-4 pb-3">
                             <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-6.jpg" alt="avatar" class="avatar rounded-circle mr-2 ml-lg-3 ml-0 z-depth-1">
                             <div class="chat-body white p-3 ml-2 z-depth-1">
                                 <div class="header">
@@ -66,114 +132,60 @@
                                     <p class="mb-0">
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                                     labore et dolore magna aliqua.
-                                </p>
-                            </div>
-                        </li>
-
-                        <li class="d-flex justify-content-between mb-4">
-                        <div class="chat-body white p-3 z-depth-1">
-                            <div class="header">
-                                <strong class="primary-font">Lara Croft</strong>
-                                <small class="pull-right text-muted"><i class="far fa-clock"></i> 13 mins ago</small>
-                            </div>
-                                <hr class="w-100">
-                                <p class="mb-0">
-                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                                laudantium.
-                            </p>
-                        </div>
-                        <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-5.jpg" alt="avatar" class="avatar rounded-circle mr-0 ml-3 z-depth-1">
-                        </li>
-
-                        <li class="d-flex justify-content-between mb-4">
-                        <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-6.jpg" alt="avatar" class="avatar rounded-circle mr-2 ml-lg-3 ml-0 z-depth-1">
-                        <div class="chat-body white p-3 ml-2 z-depth-1">
-                            <div class="header">
-                                <strong class="primary-font">Brad Pitt</strong>
-                                <small class="pull-right text-muted"><i class="far fa-clock"></i> 12 mins ago</small>
-                            </div>
-                                <hr class="w-100">
-                                <p class="mb-0">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                labore et dolore magna aliqua.
-                            </p>
-                        </div>
-                        </li>
-                    <li class="d-flex justify-content-between mb-4">
-                    <div class="chat-body white p-3 z-depth-1">
-                        <div class="header">
-                            <strong class="primary-font">Lara Croft</strong>
-                            <small class="pull-right text-muted"><i class="far fa-clock"></i> 13 mins ago</small>
-                        </div>
-                            <hr class="w-100">
-                            <p class="mb-0">
-                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                                laudantium.
-                            </p>
-                    </div>
-                    <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-5.jpg" alt="avatar" class="avatar rounded-circle mr-0 ml-3 z-depth-1">
-                    </li>
-                    <li class="d-flex justify-content-between mb-4 pb-3">
-                    <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-6.jpg" alt="avatar" class="avatar rounded-circle mr-2 ml-lg-3 ml-0 z-depth-1">
-                    <div class="chat-body white p-3 ml-2 z-depth-1">
-                        <div class="header">
-                            <strong class="primary-font">Brad Pitt</strong>
-                            <small class="pull-right text-muted"><i class="far fa-clock"></i> 12 mins ago</small>
-                        </div>
-                            <hr class="w-100">
-                            <p class="mb-0">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua.
-                            </p>
-                        </div>
-                    </li>
-                </ul>
-
-                <div class="white">
-                    <div class="form-group basic-textarea">
-                        <form method="POST" id="sendMessageForm">
-                            @csrf
-                            @method('POST')
-
-                            <div class="chat-body white p-3 z-depth-1">
-
-                                <ng-container [formGroup]="testForm">
-
-                                    <div class="custom-control custom-switch">
-                                        <input type="checkbox" class="custom-control-input" id="customSwitches" name="sendTo" formControlName="switchControl" value="class">
-                                        <label class="custom-control-label" for="customSwitches">Toggle to switch between Classes and Students</label>
-                                    </div>
-                                    <hr class="w-100">
-
-                                </ng-container>
-
-                                <label for="recipient">To the Parents of</label>
-
-                                {{-- options from script based on the radio: classes or students--}}
-                                <select name="recipient" id="recipient" required>
-                                </select><br>
-
-                                <label for="subject">Subject</label>
-                                <input type="text" name="subject" required><br>
-
-                                <div class="form-group basic-textarea">
-                                    <textarea name="description" class="form-control pl-2 my-0" id="exampleFormControlTextarea2" rows="3" placeholder="Type your message here..." required></textarea>
+                                    </p>
                                 </div>
+                            </li>
+                            -->
+                        </ul>
+
+                        <div class="white">
+                            <div class="form-group basic-textarea">
+                                <form method="POST" id="sendMessageForm">
+                                    @csrf @method('POST')
+
+                                    <div class="chat-body white p-3 z-depth-1">
+
+                                        <ng-container [formGroup]="testForm">
+
+                                            <div class="custom-control custom-switch">
+                                                <input type="checkbox" class="custom-control-input" id="customSwitches" name="sendTo" formControlName="switchControl" value="class">
+                                                <label class="custom-control-label" for="customSwitches">Toggle to switch between Classes and Students</label>
+                                            </div>
+                                            <hr class="w-100">
+
+                                        </ng-container>
+
+                                        <label for="recipient">To the Parents of</label>
+
+                                        {{-- options from script based on the radio: classes or students--}}
+                                        <select name="recipient" id="recipient" required>
+                                        </select>
+                                        <br>
+
+                                        <label for="subject">Subject</label>
+                                        <input type="text" name="subject" required>
+                                        <br>
+
+                                        <div class="form-group basic-textarea">
+                                            <textarea name="description" class="form-control pl-2 my-0" id="exampleFormControlTextarea2" rows="3" placeholder="Type your message here..." required></textarea>
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-blue btn-rounded btn-sm waves-effect waves-light float-right" name="submitHomework">Send</button>
+
+                                </form>
+
                             </div>
 
-                            <button type="submit" class="btn btn-blue btn-rounded btn-sm waves-effect waves-light float-right" name="submitHomework">Send</button>
+                        </div>
+                        <!-- Grid column -->
 
-                        </form>
+                    </div>
+                    <!-- Grid row -->
 
                 </div>
-
             </div>
-            <!-- Grid column -->
-
         </div>
-        <!-- Grid row -->
-
-    </div>
     </div>
 </div>
 @endsection
