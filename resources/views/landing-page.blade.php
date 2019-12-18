92f1138507fd1f740275bf331ba3fb0c26b84c92
kimchi <55229423+sukmabols@users.noreply.github.com>
2019-12-18 10:01:28 +0100

    indentation

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -70,11 +70,11 @@
         </div>
         <!--/.Content-->
     </div>
-    
-<!-- Modal -->
-<div class="container">
-    <!--Section: Features v.4-->
-    <section id="about" class="section wow fadeIn" data-wow-delay="0.3s">
+
+    <!-- Modal -->
+    <div class="container">
+        <!--Section: Features v.4-->
+        <section id="about" class="section wow fadeIn" data-wow-delay="0.3s">
             <!--Section heading-->
             <h1 class="font-weight-bold text-center h1 my-5">{{ __('landing-page.welcome_h_1') }}</h1>
             <!--Section description-->
@@ -92,81 +92,82 @@
                 </div>
             </div>
 
-        <hr class="mb-5">
-    </section>
-    <!--Section: Testimonials v.3-->
-    <section class="section team-section text-center pb-3 wow fadeIn" data-wow-delay="0.3s">
-        <!--Section heading-->
-        <h1 class="font-weight-bold text-center h1 my-5">{{ __('landing-page.testimonials') }}</h1>
-        <!--Section description-->
-        <p class="text-center grey-text mb-5 mx-auto w-responsive">{{ __('landing-page.tell_us_why') }}</p>
-        <!--Grid row-->
-        <div class="row text-center">
-            <!--Grid column-->
-            <div class="col-md-4 mb-4">
-                <div class="testimonial">
-                    <!--Avatar-->
-                    <div class="avatar mx-auto">
-                        <img src="../../MDB/img/jorge.jpg" class="rounded-circle z-depth-1 img-fluid">
-                    </div>
-                    <!--Content-->
-                    <h4 class="font-weight-bold mt-4 mb-3">Jorge Gonzales</h4>
-                    <h6 class="mb-3 font-weight-bold grey-text">{{ __('landing-page.profession_1') }}</h6>
-                    <p><i class="fas fa-quote-left"></i> {{ __('landing-page.testimonial_1') }} <i class="fas fa-quote-right"></i></p>
-                    <!--Review-->
-                    <div class="orange-text">
-                        <i class="fas fa-star"> </i>
-                        <i class="fas fa-star"> </i>
-                        <i class="fas fa-star"> </i>
-                        <i class="fas fa-star"> </i>
-                        <i class="fas fa-star-half-alt"> </i>
-                    </div>
-                </div>
-            </div>
-            <!--Grid column-->
-            <!--Grid column-->
-            <div class="col-md-4 mb-4">
-                <div class="testimonial">
-                    <!--Avatar-->
-                    <div class="avatar mx-auto">
-                        <img src="../../MDB/img/dennis.jpg" class="rounded-circle z-depth-1 img-fluid">
-                    </div>
-                    <!--Content-->
-                    <h4 class="font-weight-bold mt-4 mb-3">Dennis Burgers</h4>
-                    <h6 class="mb-3 font-weight-bold grey-text">{{ __('landing-page.profession_2') }}</h6>
-                    <p><i class="fas fa-quote-left"></i> {{ __('landing-page.testimonial_2') }} <i class="fas fa-quote-right"></i></p>
-                    <!--Review-->
-                    <div class="orange-text">
-                        <i class="fas fa-star"> </i>
-                        <i class="fas fa-star"> </i>
-                        <i class="fas fa-star"> </i>
-                        <i class="fas fa-star"> </i>
-                        <i class="fas fa-star"> </i>
+            <hr class="mb-5">
+        </section>
+
+        <!--Section: Testimonials v.3-->
+        <section class="section team-section text-center pb-3 wow fadeIn" data-wow-delay="0.3s">
+            <!--Section heading-->
+            <h1 class="font-weight-bold text-center h1 my-5">{{ __('landing-page.testimonials') }}</h1>
+            <!--Section description-->
+            <p class="text-center grey-text mb-5 mx-auto w-responsive">{{ __('landing-page.tell_us_why') }}</p>
+            <!--Grid row-->
+            <div class="row text-center">
+                <!--Grid column-->
+                <div class="col-md-4 mb-4">
+                    <div class="testimonial">
+                        <!--Avatar-->
+                        <div class="avatar mx-auto">
+                            <img src="../../MDB/img/jorge.jpg" class="rounded-circle z-depth-1 img-fluid">
+                        </div>
+                        <!--Content-->
+                        <h4 class="font-weight-bold mt-4 mb-3">Jorge Gonzales</h4>
+                        <h6 class="mb-3 font-weight-bold grey-text">{{ __('landing-page.profession_1') }}</h6>
+                        <p><i class="fas fa-quote-left"></i> {{ __('landing-page.testimonial_1') }} <i class="fas fa-quote-right"></i></p>
+                        <!--Review-->
+                        <div class="orange-text">
+                            <i class="fas fa-star"> </i>
+                            <i class="fas fa-star"> </i>
+                            <i class="fas fa-star"> </i>
+                            <i class="fas fa-star"> </i>
+                            <i class="fas fa-star-half-alt"> </i>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!--Grid column-->
-            <!--Grid column-->
-            <div class="col-md-4 mb-4">
-                <div class="testimonial">
-                    <!--Avatar-->
-                    <div class="avatar mx-auto">
-                        <img src="../../MDB/img/reginald.jpg" class="rounded-circle z-depth-1 img-fluid">
-                    </div>
-                    <!--Content-->
-                    <h4 class="font-weight-bold mt-4 mb-3">Reginald Wagner</h4>
-                    <h6 class="mb-3 font-weight-bold grey-text">{{ __('landing-page.profession_3') }}</h6>
-                    <p><i class="fas fa-quote-left"></i> {{ __('landing-page.testimonial_3') }} <i class="fas fa-quote-right"></i></p>
-                    <!--Review-->
-                    <div class="orange-text">
-                        <i class="fas fa-star"> </i>
-                        <i class="fas fa-star"> </i>
-                        <i class="fas fa-star"> </i>
-                        <i class="fas fa-star"> </i>
-                        <i class="far fa-star"> </i>
+                <!--Grid column-->
+                <!--Grid column-->
+                <div class="col-md-4 mb-4">
+                    <div class="testimonial">
+                        <!--Avatar-->
+                        <div class="avatar mx-auto">
+                            <img src="../../MDB/img/dennis.jpg" class="rounded-circle z-depth-1 img-fluid">
+                        </div>
+                        <!--Content-->
+                        <h4 class="font-weight-bold mt-4 mb-3">Dennis Burgers</h4>
+                        <h6 class="mb-3 font-weight-bold grey-text">{{ __('landing-page.profession_2') }}</h6>
+                        <p><i class="fas fa-quote-left"></i> {{ __('landing-page.testimonial_2') }} <i class="fas fa-quote-right"></i></p>
+                        <!--Review-->
+                        <div class="orange-text">
+                            <i class="fas fa-star"> </i>
+                            <i class="fas fa-star"> </i>
+                            <i class="fas fa-star"> </i>
+                            <i class="fas fa-star"> </i>
+                            <i class="fas fa-star"> </i>
+                        </div>
                     </div>
                 </div>
-            </div>
+                <!--Grid column-->
+                <!--Grid column-->
+                <div class="col-md-4 mb-4">
+                    <div class="testimonial">
+                        <!--Avatar-->
+                        <div class="avatar mx-auto">
+                            <img src="../../MDB/img/reginald.jpg" class="rounded-circle z-depth-1 img-fluid">
+                        </div>
+                        <!--Content-->
+                        <h4 class="font-weight-bold mt-4 mb-3">Reginald Wagner</h4>
+                        <h6 class="mb-3 font-weight-bold grey-text">{{ __('landing-page.profession_3') }}</h6>
+                        <p><i class="fas fa-quote-left"></i> {{ __('landing-page.testimonial_3') }} <i class="fas fa-quote-right"></i></p>
+                        <!--Review-->
+                        <div class="orange-text">
+                            <i class="fas fa-star"> </i>
+                            <i class="fas fa-star"> </i>
+                            <i class="fas fa-star"> </i>
+                            <i class="fas fa-star"> </i>
+                            <i class="far fa-star"> </i>
+                        </div>
+                    </div>
+                </div>
             <!--Grid column-->
         </div>
         <!--Grid row-->
@@ -233,16 +234,24 @@
             <!--Grid column-->
             <div class="col-md-4 col-xl-3">
                 <ul class="contact-icons text-center list-unstyled">
-                    <li><i class="fas fa-map-marker fa-2x orange-text"></i>
+                    <li>
+                        <i class="fas fa-map-marker fa-2x orange-text"></i>
                         <p>9, ave des Hauts-Fourneaux<br>
                             L-4362 Belval<br>
-                            Luxembourg</p>
+                            Luxembourg
+                        </p>
                     </li>
-                    <li><i class="fas fa-phone fa-2x orange-text"></i>
-                        <p>+352 691 22 36 00</p>
+                    <li>
+                        <i class="fas fa-phone fa-2x orange-text"></i>
+                        <p>
+                            +352 691 22 36 00
+                        </p>
                     </li>
-                    <li><i class="fas fa-envelope fa-2x orange-text"></i>
-                        <p>hello@mybichelchen.lu</p>
+                    <li>
+                        <i class="fas fa-envelope fa-2x orange-text"></i>
+                        <p>
+                            hello@mybichelchen.lu
+                        </p>
                     </li>
                     <li>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2591.0504564132575!2d5.946978215870579!3d49.50244116309531!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47eacad49ef04f7d%3A0x8599a1646a7921b9!2sTECHNOPORT%20SA%20%E2%80%93%20BELVAL%2C%209%20Avenue%20des%20Hauts-Fourneaux%2C%204362%20Esch%20an%20der%20Alzette!5e0!3m2!1sen!2slu!4v1575989181454!5m2!1sen!2slu" width="200" height="300" frameborder="0" style="border:2px solid orange; border-radius: 2.5px;"></iframe>
@@ -255,7 +264,6 @@
     <!--Section: Contact v.2-->
 
   </div>
-
 @endsection
 
 @section('footer')
